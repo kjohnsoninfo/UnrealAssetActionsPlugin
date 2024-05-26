10304a44cf0cdc7801dbcfe220000610c75487a7
kjohnsoninfo <kjj8781@gmail.com>
2024-05-26 18:39:36 -0500

    git user test

--- Plugins/AssetActionsManager/Source/AssetActionsManager/Public/DebugHelper.h
+++ Plugins/AssetActionsManager/Source/AssetActionsManager/Public/DebugHelper.h
@@ -4,7 +4,10 @@ namespace DebugHelper
 {
 	static void Print(const FString& Message, const FColor& Color = FColor::Green)
 	{
-
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 0.8f, Color, Message);
+		}
 	}
 
 }
