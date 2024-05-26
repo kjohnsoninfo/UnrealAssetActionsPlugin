029e6b89b3405d683bc87eeec03aafe5490dd0a7
kjohnsoninfo <kjj8781@gmail.com>
2024-05-26 18:54:53 -0500

    add debug helper fns

--- Plugins/AssetActionsManager/Source/AssetActionsManager/AssetActionsManager.Build.cs
+++ Plugins/AssetActionsManager/Source/AssetActionsManager/AssetActionsManager.Build.cs
@@ -26,6 +26,7 @@ public class AssetActionsManager : ModuleRules
 			new string[]
 			{
 				"Core",
+				"Slate"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
