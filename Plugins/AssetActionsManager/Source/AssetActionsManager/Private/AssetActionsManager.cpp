638aa6cfa0c524bce6e725dfd6afbe0891db4ba4
kjohnsoninfo <kjj8781@gmail.com>
2024-05-26 20:05:55 -0500

    add custom delete option to cb menu

--- Plugins/AssetActionsManager/Source/AssetActionsManager/Private/AssetActionsManager.cpp
+++ Plugins/AssetActionsManager/Source/AssetActionsManager/Private/AssetActionsManager.cpp
@@ -2,12 +2,14 @@
 
 #include "AssetActionsManager.h"
 #include "DebugHelper.h"
+#include "ContentBrowserModule.h" 
 
 #define LOCTEXT_NAMESPACE "FAssetActionsManagerModule"
 
 void FAssetActionsManagerModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	InitCBMenuExtension();
+	//RegisterAdvancedDeletionTab();
 }
 
 void FAssetActionsManagerModule::ShutdownModule()
@@ -15,6 +17,78 @@ void FAssetActionsManagerModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
+
+#pragma region ExtendContentBrowserMenu
+
+void FAssetActionsManagerModule::InitCBMenuExtension()
+{
+	// Load CB module
+	FContentBrowserModule& ContentBrowserModule =
+		FModuleManager::LoadModuleChecked<FContentBrowserModule>(TEXT("ContentBrowser"));
+
+	// Get array of all menu options
+	TArray<FContentBrowserMenuExtender_SelectedPaths>& ContentBrowserMenuExtenders =
+		ContentBrowserModule.GetAllPathViewContextMenuExtenders();
+
+	// Add custom option to array
+	FContentBrowserMenuExtender_SelectedPaths CustomCBMenuDelegate;
+	CustomCBMenuDelegate.BindRaw(this, &FAssetActionsManagerModule::CustomCBMenuExtender); // First delegate bind to member fn
+	ContentBrowserMenuExtenders.Add(CustomCBMenuDelegate);
+}
+
+TSharedRef<FExtender> FAssetActionsManagerModule::CustomCBMenuExtender(const TArray<FString>& SelectedPaths)
+{
+	TSharedRef<FExtender> MenuExtender(new FExtender());
+
+	if (SelectedPaths.Num() > 0)
+	{
+		MenuExtender->AddMenuExtension(FName("Delete"),
+			EExtensionHook::After,
+			TSharedPtr<FUICommandList>(),
+			FMenuExtensionDelegate::CreateRaw(this, &FAssetActionsManagerModule::AddCBMenuEntry)); // Second delegate bind to member fn
+
+		// Selected Paths is passed in through bind fn which is based on user selection of folder
+		// Create an array and set it to the path that is obtained from this menu action
+		SelectedFolderPaths = SelectedPaths;
+	}
+
+	return MenuExtender;
+}
+
+void FAssetActionsManagerModule::AddCBMenuEntry(FMenuBuilder& MenuBuilder)
+{
+	MenuBuilder.AddMenuEntry
+	(
+		FText::FromString("Advanced Deletion"), // Name
+		FText::FromString("Search and delete assets under a selected folder"), // ToolTip
+		FSlateIcon(),
+		FExecuteAction::CreateRaw(this, &FAssetActionsManagerModule::OnAdvancedDeleteButtonClicked) // Third delegate bind to member fn
+	);
+}
+
+void FAssetActionsManagerModule::OnAdvancedDeleteButtonClicked()
+{
+	DebugHelper::Print("Button clicked");
+	// Spawn tab 
+}
+
+#pragma endregion
+
+#pragma region AdvancedDeletionTab
+
+//void FAssetActionsManagerModule::RegisterAdvancedDeletionTab()
+//{
+//	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(FName("AdvancedDeletion"),
+//		FOnSpawnTab::CreateRaw(this, &FAssetActionsManagerModule::OnSpawnAdvancedDeletionTab))
+//		.SetDisplayName(FText::FromString(TEXT("Advanced Deletion")));
+//}
+//
+//TSharedRef<SDockTab> FAssetActionsManagerModule::OnSpawnAdvancedDeletionTab(const FSpawnTabArgs& AdvancedDeletionTabArgs)
+//{
+//	return TSharedRef<SDockTab>();
+//}
+
+#pragma endregion
 
 #undef LOCTEXT_NAMESPACE
 	
--- Plugins/AssetActionsManager/Source/AssetActionsManager/Public/AssetActionsManager.h
+++ Plugins/AssetActionsManager/Source/AssetActionsManager/Public/AssetActionsManager.h
@@ -12,4 +12,25 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+#pragma region ExtendContentBrowserMenu
+	
+	TArray<FString> SelectedFolderPaths;
+
+	void InitCBMenuExtension();
+	TSharedRef<FExtender> CustomCBMenuExtender(const TArray<FString>& SelectedPaths);
+	void AddCBMenuEntry(FMenuBuilder& MenuBuilder);
+	void OnAdvancedDeleteButtonClicked();
+
+#pragma endregion
+
+#pragma region AdvancedDeletionTab
+
+	//void RegisterAdvancedDeletionTab();
+
+	//TSharedRef<SDockTab> OnSpawnAdvancedDeletionTab(const FSpawnTabArgs& AdvancedDeletionTabArgs);
+
+#pragma endregion
+
 };
